a9e17e8f08fdbb22b5de8e4fed3ed6b8be4a5217
victoria magno <127114908+victoriamagno@users.noreply.github.com>
2024-08-08 10:47:23 -0300

    Update aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Ana", "Gabriela", "Marina", "Marcel", "Alice", "Gian", "Gael"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
